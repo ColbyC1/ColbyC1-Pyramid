f7e33bc1a39e9dd1e4230131b2f1342c71d1511b
Colby Canselo <colbycanselo@gmail.com>
2023-03-02 20:13:51 +0000

    PYRAMID

--- src/main/java/Pyramid.java
+++ src/main/java/Pyramid.java
@@ -1,33 +1,46 @@
 
 public class Pyramid {
     /**
-     * TODO: Return a string that is a pyramid of size n, made of of asterisks and no spaces.
+     * TODO: Return a string that is a pyramid of size n, made of of asterisks and
+     * no spaces.
      *
-     *     for example:
+     * for example:
      *
-     *     size 3:
-     *     *
-     *     **
-     *     ***
+     * size 3:
+     * *
+     * **
+     * ***
      *
-     *     size 5:
-     *     *
-     *     **
-     *     ***
-     *     ****
-     *     *****
+     * size 5:
+     * *
+     * **
+     * ***
+     * ****
+     * *****
      *
      * The newline character can be written as \n .
-     * The tests will not be affected by a newline character at the beginning or end of the entire pyramid.
+     * The tests will not be affected by a newline character at the beginning or end
+     * of the entire pyramid.
      *
-     * Here's a hint: you could use a for loop inside of a for loop for this problem. The outer for loop
-     * can determine the current 'row', and the inner for loop can print an asterisk an amount of times based on the
+     * Here's a hint: you could use a for loop inside of a for loop for this
+     * problem. The outer for loop
+     * can determine the current 'row', and the inner for loop can print an asterisk
+     * an amount of times based on the
      * current 'row'.
      *
      * @param n the size of the pyramid.
      * @return a string representation of the pyramid.
      */
-    public String returnPyramid(int n){
-        return "";
+    public String returnPyramid(int n) {
+        String blank = "";
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                blank+="*";
+            }
+            blank+="\n";
+        }
+
+        return blank;
     }
 }
